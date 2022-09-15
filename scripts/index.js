03fef78fe4d5fb32a209e0c00e1b43a5bac63e5c
ulisesBasurto <ulisesbasurto01@gmail.com>
2022-09-15 11:24:15 -0500

    feat: add timeout for close tnks modal

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,9 @@ if (form) {
     tnksModal.style.display = 'block';
     tnksModal.style.paddingRight = "17px";
     tnksModal.className = "modal fade show";
-
+    setTimeout(() => {
+        tnksModal.style.display = "none";
+        tnksModal.className = "modal fade";
+    }, 4000)
 
 } else console.log('sin param');
